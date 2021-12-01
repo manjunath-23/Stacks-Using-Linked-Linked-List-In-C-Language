9336e8504a5798db822a8f6fe6e6e7606c4cb6b5
Manjunath <91129998+manjunath-23@users.noreply.github.com>
2021-12-01 12:05:28 +0530

    Update and rename index.md to Program.c

---
+++ docs/Program.c
@@ -0,0 +1,210 @@
+## Welcome to GitHub Pages
+
+You can use the [editor on GitHub](https://github.com/manjunath-23/Stacks-Using-Linked-Linked-List-In-C-Language.c/edit/main/docs/index.md) to maintain and preview the content for your website in Markdown files.
+
+Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
+
+### Markdown
+
+Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
+
+```markdown
+Syntax highlighted code block
+
+# Header 1
+## Header 2
+### Header 3
+
+- Bulleted
+- List
+* C Program to Implement a Stack using Linked List
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *ptr;
+}*top,*top1,*temp;
+ 
+int topelement();
+void push(int data);
+void pop();
+void empty();
+void display();
+void destroy();
+void stack_count();
+void create();
+int count = 0;
+void main()
+{
+    int no, ch, e;
+    printf("\n 1 - Push");
+    printf("\n 2 - Pop");
+    printf("\n 3 - Top");
+    printf("\n 4 - Empty");
+    printf("\n 5 - Exit");
+    printf("\n 6 - Dipslay");
+    printf("\n 7 - Stack Count");
+    printf("\n 8 - Destroy stack");
+    create();
+    while (1)
+    {
+        printf("\n Enter choice : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("Enter data : ");
+            scanf("%d", &no);
+            push(no);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            if (top == NULL)
+                printf("No elements in stack");
+            else
+            {
+                e = topelement();
+                printf("\n Top element : %d", e);
+            }
+            break;
+        case 4:
+            empty();
+            break;
+        case 5:
+            exit(0);
+        case 6:
+            display();
+            break;
+        case 7:
+            stack_count();
+            break;
+        case 8:
+            destroy();
+            break;
+        default :
+            printf(" Wrong choice, Please enter correct choice  ");
+            break;
+        }
+    }
+}
+ 
+/* Create empty stack */
+void create()
+{
+    top = NULL;
+}
+ 
+/* Count stack elements */
+void stack_count()
+{
+    printf("\n No. of elements in stack : %d", count);
+}
+ 
+/* Push data into stack */
+void push(int data)
+{
+    if (top == NULL)
+    {
+        top =(struct node *)malloc(1*sizeof(struct node));
+        top->ptr = NULL;
+        top->info = data;
+    }
+    else
+    {
+        temp =(struct node *)malloc(1*sizeof(struct node));
+        temp->ptr = top;
+        temp->info = data;
+        top = temp;
+    }
+    count++;
+}
+ 
+/* Display stack elements */
+void display()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("Stack is empty");
+        return;
+    }
+ 
+    while (top1 != NULL)
+    {
+        printf("%d ", top1->info);
+        top1 = top1->ptr;
+    }
+ }
+ 
+/* Pop Operation on stack */
+void pop()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("\n Error : Trying to pop from empty stack");
+        return;
+    }
+    else
+        top1 = top1->ptr;
+    printf("\n Popped value : %d", top->info);
+    free(top);
+    top = top1;
+    count--;
+}
+ 
+/* Return top element */
+int topelement()
+{
+    return(top->info);
+}
+ 
+/* Check if stack is empty or not */
+void empty()
+{
+    if (top == NULL)
+        printf("\n Stack is empty");
+    else
+        printf("\n Stack is not empty with %d elements", count);
+}
+ 
+/* Destroy entire stack */
+void destroy()
+{
+    top1 = top;
+ 
+    while (top1 != NULL)
+    {
+        top1 = top->ptr;
+        free(top);
+        top = top1;
+        top1 = top1->ptr;
+    }
+    free(top1);
+    top = NULL;
+ 
+    printf("\n All stack elements destroyed");
+    count = 0;
+}
+1. Numbered
+2. List
+
+**Bold** and _Italic_ and `Code` text
+
+[Link](url) and ![Image](src)
+```
+
+For more details see [Basic writing and formatting syntax](https://docs.github.com/en/github/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax).
+
+### Jekyll Themes
+
+Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/manjunath-23/Stacks-Using-Linked-Linked-List-In-C-Language.c/settings/pages). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
+
+### Support or Contact
+
+Having trouble with Pages? Check out our [documentation](https://docs.github.com/categories/github-pages-basics/) or [contact support](https://support.github.com/contact) and we’ll help you sort it out.
